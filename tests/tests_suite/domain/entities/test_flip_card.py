b36ec543ef0dc6fc545d044529b3e459648e02a7
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-04-25 21:05:09 +0200

    refactor: flip card test

--- tests/tests_suite/domain/entities/test_flip_card.py
+++ tests/tests_suite/domain/entities/test_flip_card.py
@@ -2,10 +2,19 @@ from unittest import TestCase
 from uuid import uuid4
 
 from src.domain.entities.flip_card import FlipCard
-from tests.mocks.domain.vos.SentenceBackStub import SentenceBackStub
-from tests.mocks.domain.vos.SentenceFrontStub import SentenceFrontStub
+from tests.test_doubles.domain.vos.SentenceBackStub import SentenceBackStub
+from tests.test_doubles.domain.vos.SentenceFrontStub import SentenceFrontStub
 
 
 class TestFlipCard(TestCase):
-    def test_instance(self):
-        self.flip_card = FlipCard(id_=uuid4(), front=SentenceFrontStub(), back=SentenceBackStub())
+    def test_flip_card_creation__return_flip_card(self):
+        front_stub = SentenceFrontStub()
+        back_stub = SentenceBackStub()
+        uuid_ = uuid4()
+
+        flip_card = FlipCard(id_=uuid_, front=front_stub, back=back_stub)
+
+        self.assertIsInstance(flip_card, FlipCard)
+        self.assertEqual(flip_card.front, front_stub)
+        self.assertEqual(flip_card.back, back_stub)
+        self.assertEqual(flip_card.id_, uuid_)
