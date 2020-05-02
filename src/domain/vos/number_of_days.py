5f1cd208283964fc15bb8a314db8e4fda25c5e48
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-05-02 16:20:06 +0200

    feat: number_of_days validation

--- src/domain/vos/number_of_days.py
+++ src/domain/vos/number_of_days.py
@@ -2,5 +2,9 @@ from src.domain.vos.abstractions.value_object import ValueObject
 
 
 class NumberOfDays(ValueObject):
-    def _validate_value(self, value):
-        """pass"""  # TODO implement
+    def _validate_value(self, value: int):
+        if isinstance(value, int) and 31 > value > 0:
+            raise self.InvalidNumberOfDays()
+
+    class InvalidNumberOfDays(Exception):
+        """pass"""
