3ea57eb58011f253a21f17d25063b41512474764
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-06-06 21:33:35 +0200

    reset: remove old flip card plans concept

--- src/domain/vos/flip_card_side_id.py
+++
@@ -1,9 +0,0 @@
-from src.domain.vos.abstractions.uuid_value_object import UuidValueObject
-
-
-class FlipCardSideId(UuidValueObject):
-    def _raise_exception(self):
-        raise self.InvalidFlipCardSideId
-
-    class InvalidFlipCardSideId(Exception):
-        """pass"""
