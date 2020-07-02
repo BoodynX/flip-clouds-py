7cb8b09c5677636fdc98241b7d567e6575e629a9
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-07-02 20:47:51 +0200

    refactor: flip_card_manager test

--- tests/system/http/contexts/flip_card_manager/test_flip_card_manager.py
+++ tests/system/http/contexts/flip_card_manager/test_flip_card_manager.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from src.interface.http.contexts.flip_card_manager.routes import ADD_CARD
 from tests.system.http.abstractions.system_test import SystemTest
 
@@ -14,7 +16,11 @@ class TestFlipCardManager(SystemTest):
             json=json_payload
         )
 
-        response_json = response.json()
         self.assertEqual(response.status_code, 201)
+        self._assert_valid_response_payload(json_payload, response)
+
+    def _assert_valid_response_payload(self, json_payload, response):
+        response_json = response.json()
+        self.assertTrue(UUID(response_json['id_'], version=4))
         self.assertEqual(response_json['front'], json_payload['front'])
         self.assertEqual(response_json['back'], json_payload['back'])
